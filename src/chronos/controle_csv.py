f16ecf1bc0adffbd6de77d658f6dbda6ae1ee21c
Jorge Guimaraes <jmguimaraes@id.uff.br>
2020-11-27 23:45:36 +0000

    feat: try e except adicionado

--- src/chronos/controle_csv.py
+++ src/chronos/controle_csv.py
@@ -6,9 +6,14 @@ class ControleCsv():
     """
     
     def get_dict_dados(self, caminho):
-        lista_de_dados = []
-        with open(caminho, newline='') as arquivo_csv:
-            dados = csv.DictReader(arquivo_csv)
-            for linha in dados:
-                lista_de_dados.append(linha)
-        return lista_de_dados
+        try:
+            lista_de_dados = []
+            with open(caminho, newline='') as arquivo_csv:
+                dados = csv.DictReader(arquivo_csv)
+                for linha in dados:
+                    lista_de_dados.append(linha)
+            return lista_de_dados
+
+        except IOError:
+            lista_de_dados.append({'erro':'arquivo nao encontrado'})
+            return lista_de_dados
