24da00a7f600f98b6418d5fb7f352ad2d8e83c9c
Jorge Guimaraes <jmguimaraes@id.uff.br>
2020-11-27 23:26:06 +0000

    pass: get_dict_dados

--- src/chronos/controle_csv.py
+++ src/chronos/controle_csv.py
@@ -1,8 +1,14 @@
-
+import csv
 
 class ControleCsv():
     """
     Opera e manipula dados csv
     """
     
-    def get_dict_dados(self, caminho): return
+    def get_dict_dados(self, caminho):
+        lista_de_dados = []
+        with open(caminho, newline='') as arquivo_csv:
+            dados = csv.DictReader(arquivo_csv)
+            for linha in dados:
+                lista_de_dados.append(linha)
+        return lista_de_dados
