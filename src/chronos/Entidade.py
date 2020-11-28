b6004306739733f2779013654bcdd4bfeb8d790d
Jorge Guimaraes <jmguimaraes@id.uff.br>
2020-11-28 19:45:20 +0000

    fix: metodos get removidos

--- src/chronos/Entidade.py
+++ src/chronos/Entidade.py
@@ -1,29 +1,20 @@
 
-class Aluno():
+class Entidade():
     """
     Guarda informacoes gereais sobre o aluno
     """
-    def __init__(self, matricula):
-        self.matricula              = matricula
+    def __init__(self, id):
+        self.id                     = id
         self.nota_ponderada         = 0
         self.carga_horaria_total    = 0
         return
-    
-    def get_matricula(self):
-        return self.matricula
-    
+       
     def set_nota_ponderada(self, nota, carga):
         self.nota_ponderada += nota * carga
         return
     
-    def get_nota_ponderada(self):
-        return self.nota_ponderada
-
     def set_carga_horaria_total(self, carga):
         self.carga_horaria_total += carga
-
-    def get_carga_horaria_total(self):
-        return self.carga_horaria_total
 
     def get_cr(self):
         if self.carga_horaria_total == 0:
