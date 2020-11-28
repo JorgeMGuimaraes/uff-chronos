8832383853e0fc03c2d6130df8d74030c9b6fea7
Jorge Guimaraes <jmguimaraes@id.uff.br>
2020-11-28 21:15:18 +0000

    feat: teste de  arquivo com cabecalho correto

--- src/chronos/Controle_csv.py
+++ src/chronos/Controle_csv.py
@@ -9,6 +9,12 @@ class ControleCsv():
         try:
             lista_de_dados = []
             with open(caminho, newline='') as arquivo_csv:
+
+                cabecalho = 'MATRICULA,COD_DISCIPLINA,COD_CURSO,NOTA,CARGA_HORARIA,ANO_SEMESTRE' 
+                if not cabecalho in arquivo_csv.readline():
+                    return lista_de_dados
+
+                arquivo_csv.seek(0)
                 dados = csv.DictReader(arquivo_csv)
                 for linha in dados:
                     lista_de_dados.append(linha)
