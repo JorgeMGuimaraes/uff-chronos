3d500e824edebfac8b5b9a6943189faeeb2fe5b8
Jorge Guimaraes <jmguimaraes@id.uff.br>
2020-11-28 20:41:28 +0000

    pass: executa com sucesso

--- src/chronos/Main_cli.py
+++ src/chronos/Main_cli.py
@@ -1,3 +1,27 @@
+import argparse
+from pathlib import Path
+from Controle_entidade import ControleEntidade
+from Controle_csv import ControleCsv
 
 if __name__ == "__main__":
-    pass    
+    parser  = argparse.ArgumentParser(description='Processa um dataset e imprime Crs.')
+    parser.add_argument("caminho", type=Path)
+    args    = parser.parse_args()
+    if not args.caminho.exists():
+        exit()
+    
+    controle_csv        = ControleCsv()
+    controle_entidade   = ControleEntidade()
+    dados               = controle_csv.get_dict_dados(args.caminho)
+    if len(dados) == 0:
+        exit()
+    
+    alunos  = dict()
+    cursos  = dict()
+
+    for linha in dados:
+        controle_entidade.preenche(alunos, 'MATRICULA', linha)
+        controle_entidade.preenche(cursos, 'COD_CURSO', linha)
+
+    controle_entidade.imprime_cli(alunos, '------- O CR dos alunos é: --------')
+    controle_entidade.imprime_cli(cursos, '----- Média de CR dos cursos: -----')
