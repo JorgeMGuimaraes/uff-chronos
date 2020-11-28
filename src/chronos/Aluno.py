5dc86c9dcffe5c9ac465d686a624b36537cbc4a1
Jorge Guimaraes <jmguimaraes@id.uff.br>
2020-11-28 01:18:26 +0000

    metodos adicionados

--- src/chronos/Aluno.py
+++ src/chronos/Aluno.py
@@ -3,4 +3,10 @@ class Aluno():
     """
     Guarda informacoes gereais sobre o aluno
     """
-    pass
+    def get_matricula(self): pass
+
+    def get_nota_ponderada(self): pass
+
+    def get_carga_horaria_total(self): pass
+
+    def get_cr(self): pass
